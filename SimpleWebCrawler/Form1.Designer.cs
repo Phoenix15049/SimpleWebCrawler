0106a17547ff1116dc30e151e52ada8f36b771ae
Mohammad Reza Yadollahi <80624792+Phoenix15049@users.noreply.github.com>
2023-02-23 01:18:38 +0330

    main crawler added.

--- SimpleWebCrawler/Form1.Designer.cs
+++ SimpleWebCrawler/Form1.Designer.cs
@@ -28,20 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.Btn_Crawler = new System.Windows.Forms.Button();
+            this.Weblink = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // Btn_Crawler
+            // 
+            this.Btn_Crawler.Location = new System.Drawing.Point(268, 239);
+            this.Btn_Crawler.Name = "Btn_Crawler";
+            this.Btn_Crawler.Size = new System.Drawing.Size(197, 68);
+            this.Btn_Crawler.TabIndex = 0;
+            this.Btn_Crawler.Text = "Crawl!";
+            this.Btn_Crawler.UseVisualStyleBackColor = true;
+            this.Btn_Crawler.Click += new System.EventHandler(this.Btn_Crawler_Click);
+            // 
+            // Weblink
+            // 
+            this.Weblink.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Weblink.Location = new System.Drawing.Point(252, 110);
+            this.Weblink.Name = "Weblink";
+            this.Weblink.Size = new System.Drawing.Size(236, 34);
+            this.Weblink.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(765, 450);
+            this.Controls.Add(this.Weblink);
+            this.Controls.Add(this.Btn_Crawler);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Btn_Crawler;
+        private System.Windows.Forms.TextBox Weblink;
     }
 }
 
--- SimpleWebCrawler/Form1.cs
+++ SimpleWebCrawler/Form1.cs
@@ -7,7 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using Abot2.Core;
+using Abot2.Crawler;
+using Abot2.Poco;
+using Serilog;
 namespace SimpleWebCrawler
 {
     public partial class Form1 : Form
@@ -15,6 +18,67 @@ namespace SimpleWebCrawler
         public Form1()
         {
             InitializeComponent();
+        }
+        
+        private void Btn_Crawler_Click(object sender, EventArgs e)
+        {
+         
+            string[] args = { "0", "1" };
+            string url = Weblink.Text;
+            Main(args, url);
+        }
+
+
+        static async Task Main(string[] args,string url)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            Log.Logger.Information("Demo starting up!");
+
+            await DemoSimpleCrawler(url);
+            await DemoSinglePageRequest();
+        }
+
+        private static async Task DemoSimpleCrawler(string url)
+        {
+            var config = new CrawlConfiguration
+            {
+                MaxPagesToCrawl = 10, //Only crawl 10 pages
+                MinCrawlDelayPerDomainMilliSeconds = 3000 //Wait this many millisecs between requests
+            };
+            var crawler = new PoliteWebCrawler(config);
+
+            crawler.PageCrawlCompleted += PageCrawlCompleted;//Several events available...
+
+            var crawlResult = await crawler.CrawlAsync(new Uri(url));
+        }
+
+        private static async Task DemoSinglePageRequest()
+        {
+            var pageRequester = new PageRequester(new CrawlConfiguration(), new WebContentExtractor());
+
+            var crawledPage = await pageRequester.MakeRequestAsync(new Uri("http://google.com"));
+            Log.Logger.Information("{result}", new
+            {
+                url = crawledPage.Uri,
+                status = Convert.ToInt32(crawledPage.HttpResponseMessage.StatusCode)
+            });
+        }
+
+        private static void PageCrawlCompleted(object sender, PageCrawlCompletedArgs e)
+        {
+            var httpStatus = e.CrawledPage.HttpResponseMessage.StatusCode;
+            var rawPageText = e.CrawledPage.Content.Text;
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
         }
     }
 }
