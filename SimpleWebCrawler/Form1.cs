4a64cb34374243d41fdaa9a056f5d65b72bb7f40
Mohammad Reza Yadollahi <80624792+Phoenix15049@users.noreply.github.com>
2023-03-21 23:15:34 +0330

    some bugs fixed.

--- SimpleWebCrawler/Form1.cs
+++ SimpleWebCrawler/Form1.cs
@@ -38,54 +38,6 @@ namespace SimpleWebCrawler
         }
 
 
-        static async Task Main(string[] args,string url)
-        {
-            Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Information()
-                .WriteTo.Console()
-                .CreateLogger();
-            
-            Log.Logger.Information("Demo starting up!");
-            
-
-            await DemoSimpleCrawler(url);
-            await DemoSinglePageRequest();
-        }
-
-        private static async Task DemoSimpleCrawler(string url)
-        {
-            var config = new CrawlConfiguration
-            {
-                MaxPagesToCrawl = 20, //Only crawl 10 pages
-                MinCrawlDelayPerDomainMilliSeconds = 3000 //Wait this many millisecs between requests
-            };
-            var crawler = new PoliteWebCrawler(config);
-
-            crawler.PageCrawlCompleted += PageCrawlCompleted;//Several events available...
-
-            var crawlResult = await crawler.CrawlAsync(new Uri(url));
-        }
-
-        private static async Task DemoSinglePageRequest()
-        {
-            var pageRequester = new PageRequester(new CrawlConfiguration(), new WebContentExtractor());
-
-            var crawledPage = await pageRequester.MakeRequestAsync(new Uri("http://google.com"));
-            Log.Logger.Information("{result}", new
-            {
-                
-                url = crawledPage.Uri,
-                status = Convert.ToInt32(crawledPage.HttpResponseMessage.StatusCode)
-            });
-        }
-
-        private static void PageCrawlCompleted(object sender, PageCrawlCompletedArgs e)
-        {
-            var httpStatus = e.CrawledPage.HttpResponseMessage.StatusCode;
-            var rawPageText = e.CrawledPage.Content.Text;//HTML
-            
-        }
-
 
 
         private void Form1_Load(object sender, EventArgs e)
